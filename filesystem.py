480a23d967682b38266c274fe53c2550cab215fe
hchap1 <hchap1@hotmail.com>
2024-12-20 09:50:19 +1000

    added duplication detection with 0 byte file support

--- filesystem.py
+++ filesystem.py
@@ -1,4 +1,13 @@
-from os import path, listdir
+from os import path, listdir, stat
+
+class DebugTracker:
+    def __init__(self):
+        self.files = 0
+        self.directories = 0
+        self.totalbytes = 0
+
+    def log(self):
+        print(f"\r[INFORMATION] {self.files} files scanned totalling {self.totalbytes} over {self.directories} directories.", end="")
 
 class Directory:
     def __init__(self, path: str):
@@ -7,6 +16,7 @@ class Directory:
 
 class File:
     def __init__(self, abspath: str, filename: str, extension: str):
+        self.ignore = False
         self.path = abspath
         self.filename = filename
         self.extension = extension
@@ -21,17 +31,25 @@ class Checksum:
         for num in binary:
             self.sum += num
         if len(binary) == 0:
-            print(f"Zero-byte file: {file.absolute}")
+            print(f"[ERROR] Zero-byte file at {file.absolute}")
+            file.ignore = True
+            return
         self.average = int(self.sum / len(binary))
         self.value = int((self.sum + self.average) / 100)
 
-def recursively_walk_directories(root: Directory, dump):
+def recursively_walk_directories(root: Directory, dump, debug):
+    debug.directories += 1
     for item in listdir(root.path):
         item_path = path.join(root.path, item)
-        if path.isdir(item_path): recursively_walk_directories(Directory(item_path), dump)
+        if path.isdir(item_path): recursively_walk_directories(Directory(item_path), dump, debug)
         elif path.isfile(item_path):
-            filename, extension = path.splitext(item)
-            root.files.append(File(path.abspath(item_path).strip(item), filename, extension))
+            size = stat(item_path).st_size
+            if size > 1:
+                debug.files += 1
+                debug.totalbytes += size
+                filename, extension = path.splitext(item)
+                root.files.append(File(path.abspath(item_path).strip(item), filename, extension))
+                debug.log()
     dump.append(root)
 
 def middle_10_percent(data):
@@ -48,15 +66,20 @@ if __name__ == "__main__":
     roots = []
     while True:
         root = input("Enter Directory [ENTER] to cancel.\n -> ")
-        if root == "": break
+        if root == "\n" or root == "": break
         if path.isdir(root): roots.append(Directory(root))
         else: print("[ERROR] Invalid directory.")
 
     directories = []
     checksums = {}
+    debug = DebugTracker()
+
+    print("[CHECKPOINT] Starting discovery of files.")
 
     for root in roots:
-        recursively_walk_directories(root, directories)
+        recursively_walk_directories(root, directories, debug)
+
+    print("\n[CHECKPOINT] Finished discovery of files.")
 
     duplicates = []
 
@@ -66,10 +89,8 @@ if __name__ == "__main__":
                 if len(checksums[file.checksum.value].filename) > len(file.filename):
                     duplicates.append((checksums[file.checksum.value].absolute, file.absolute))
                     checksums[file.checksum.value] = file
-                else:
-                    duplicates.append((file.absolute, checksums[file.checksum.value].absolute))
-            else:
-                checksums[file.checksum.value] = file
+                else: duplicates.append((file.absolute, checksums[file.checksum.value].absolute))
+            else: checksums[file.checksum.value] = file
 
     for duplicate in duplicates:
-        print(f"[DUPLICATE] {duplicate[0].absolute} is a copy of {duplicate[1].absolute}")
+        print(f"[DUPLICATE] {duplicate[0]} is a copy of {duplicate[1]}")
